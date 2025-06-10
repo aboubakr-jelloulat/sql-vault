fd775f0b5590a99d609a952efa52421fde7f8ed4
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-10 16:27:21 +0100

    Count BMW X5 Models

--- SQL - Practice/1_count.sql
+++ SQL - Practice/1_count.sql
@@ -1,5 +1,5 @@
 
--- Problem 1: Count how many cars in the "Cars" table are the X5 model
+-- Problem 1 : Count how many cars in the "Cars" table are the X5 model
         
 select count (*) from Cars
     where Model = 'X5';
