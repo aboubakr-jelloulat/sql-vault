75f7864ed7170229966b80c3c009ab4186d0bba2
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-14 10:08:04 +0100

    Cast

--- SQL - Practice/7.sql
+++ SQL - Practice/7.sql
@@ -1,3 +1,5 @@
+-- Problem 7: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's percentage
+
 select * , cast ([Number Of Cars] as float) / cast ([Total Vehicles] as float ) as Perc from
 (
 	select Makes.Make, count (*) as [Number Of Cars], (select count (*) from VehicleDetails) as [Total Vehicles] from VehicleDetails
