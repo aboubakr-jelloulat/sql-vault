dcf58e129b9fe000a0c826c3ff5de132b1fc09a6
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-14 11:10:49 +0100

    distinct : remove duplicates from the result

---
+++ SQL - Practice/10.sql
@@ -0,0 +1,10 @@
+--   Problem 9:  Problem 9: Get all vehicles that runs with GAS
+
+
+select  distinct  Makes.Make, FuelTypes.FuelTypeName from VehicleDetails
+
+	inner join Makes on VehicleDetails.MakeID = Makes.MakeID
+	inner join FuelTypes on VehicleDetails.FuelTypeID = FuelTypes.FuelTypeID
+
+		Where FuelTypes.FuelTypeName = N'GAS'
+		order by Makes.Make
