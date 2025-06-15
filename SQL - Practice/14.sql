e6275321452979c33b8d5b7e40f9a0ada1bb02d4
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-15 09:52:29 +0100

    Get all  Vehicles with make starts with 'B'

---
+++ SQL - Practice/14.sql
@@ -0,0 +1,7 @@
+--   Problem 14: Get all Makes with make starts with 'B'
+
+--Get all  Vehicles with make starts with 'B'
+
+SELECT        Makes.Make
+FROM            Makes
+where Makes.Make like 'B%';
