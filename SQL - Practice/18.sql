5bc3f81aa0467f39bbcef4bf763a718e76acc5c1
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-16 20:44:32 +0100

    problem 18

--- SQL - Practice/18.sql
+++ SQL - Practice/18.sql
@@ -3,11 +3,17 @@
 
 --Get total vehicles per DriveTypeName Per Make and order them per make asc then per total Desc
 
-SELECT        distinct Makes.Make, DriveTypes.DriveTypeName, Count(*) AS Total
-FROM            DriveTypes INNER JOIN
-                         VehicleDetails ON DriveTypes.DriveTypeID = VehicleDetails.DriveTypeID INNER JOIN
-                         Makes ON VehicleDetails.MakeID = Makes.MakeID
-
-Group By Makes.Make, DriveTypes.DriveTypeName
-
-Order By Make ASC, Total Desc
+SELECT DISTINCT
+    Makes.Make,
+    DriveTypes.DriveTypeName,
+    COUNT(*) AS Total
+FROM
+    DriveTypes
+    INNER JOIN VehicleDetails ON DriveTypes.DriveTypeID = VehicleDetails.DriveTypeID
+    INNER JOIN Makes ON VehicleDetails.MakeID = Makes.MakeID
+GROUP BY
+    Makes.Make,
+    DriveTypes.DriveTypeName
+ORDER BY
+    Make ASC,
+    Total DESC
