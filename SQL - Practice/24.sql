a9b4ef3c2f1794dfec034b105f4cdc7f9204dbbe
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-18 18:46:30 +0100

    problem 24

---
+++ SQL - Practice/24.sql
@@ -0,0 +1,7 @@
+
+-- Problem 24: Get all vehicles that have Engines > 3 Liters and have only 2 doors
+
+-- Get all vehicles that have Engines > 3 Liters and have only 2 doors
+
+select * from VehicleDetails
+	where Engine_Liter_Display >  3 and NumDoors = 2;
