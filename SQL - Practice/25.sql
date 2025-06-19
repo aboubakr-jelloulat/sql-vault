03acbc22efe56813cf3a66ede39896a9b7237d92
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-19 11:23:58 +0100

    %xx%  VehicleDetails.*

---
+++ SQL - Practice/25.sql
@@ -0,0 +1,7 @@
+
+-- Problem 25: Get make and vehicles that the engine contains 'OHV' and have Cylinders = 4
+
+SELECT         Makes.Make, VehicleDetails.*
+FROM            VehicleDetails
+INNER JOIN	Makes ON VehicleDetails.MakeID = Makes.MakeID
+WHERE        (VehicleDetails.Engine LIKE '%OHV%') AND (VehicleDetails.Engine_Cylinders = 4)
