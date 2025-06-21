83d193f2023166336dcc854ac93a2fddc41c8ef5
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-21 11:54:08 +0100

    unique = removes duplicates  = distinct

---
+++ SQL - Practice/38.sql
@@ -0,0 +1,11 @@
+
+ -- Problem 38: Get all unique Engin_CC and sort them Desc
+
+
+ select  distinct  Engine_CC from VehicleDetails
+
+	order by Engine_CC desc
+
+
+
+-- SELECT DISTINCT Engin_CC: returns only unique values (removes duplicates)
