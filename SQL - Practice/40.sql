51fbdea89764fc2793771eb614662cd18ad8fd44
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-22 20:35:36 +0100

    Get all vehicles that has one of the Max 3 Engine CC trick : top 3 student ...

---
+++ SQL - Practice/40.sql
@@ -0,0 +1,12 @@
+
+
+--  Problem 40: Get all vehicles that has one of the Max 3 Engine CC
+
+
+select Vehicle_Display_Name  from VehicleDetails
+	where Engine_CC in
+	(
+		select top 3 Engine_CC from VehicleDetails
+		order by Engine_CC desc
+	)
+	
