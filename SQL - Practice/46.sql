03acd0f7700f8931c2c799bb469bd77ca56f70d4
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-23 21:19:46 +0100

    top 3

--- SQL - Practice/46.sql
+++ SQL - Practice/46.sql
@@ -7,4 +7,3 @@ select distinct  top 3  Makes.Make, count (*) as [Number of Models]  from Makes
 	inner join MakeModels on MakeModels.MakeID = Makes.MakeID
 	group by Makes.Make
 	order by [Number of Models] desc
-	
