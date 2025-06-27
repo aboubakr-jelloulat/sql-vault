e54c0bfe58c61bdc4a4ada0d42a8d21401a0c787
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-27 11:41:37 +0100

    left join

--- SQL - Practice/Self Referential Queries/52.sql
+++ SQL - Practice/Self Referential Queries/52.sql
@@ -1,14 +1,12 @@
 
---   Problem 51: Get all employees that have manager along with Manager's name.
+
+--  Problem 52: Get all employees that have manager or does not have manager along with Manager's name, incase no manager name show null
+
+-- Get all employees that have manager or does not have manager along with Manager's name, incase no manager name show null
+-- this will select all data from employees regardless if they have manager or not, note here we used left outer join
 
 
--- Get all employees that have manager along with Manager's name.
--- this will select all data from employees that are managed by someone along with their manager name,
--- employees that have no manager will not be selected because we used inner join
--- Note we used inner join on the same table with diffrent alliace.
+SELECT        Employees.Name, Employees.ManagerID, Employees.Salary, Managers.Name AS ManagerName FROM   Employees
 
-
-
-select Employees.Name, Employees.ManagerID, Employees.Salary, Employees.ManagerID, Managers.Name as [Manager Name] from Employees
-
-	inner join Employees As Managers ON Employees.ManagerID = Managers.ManagerID
+	Left JOIN	Employees AS Managers ON Employees.ManagerID = Managers.EmployeeID
+	
