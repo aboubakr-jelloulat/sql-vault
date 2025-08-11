d454f6aea4ffb4349c56d2a7a7a7a0559856a9b8
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-08-11 11:53:22 +0100

    Intro into CASE Statments

--- T-SQL/02-CASE Statement/00_intro.sql
+++ T-SQL/02-CASE Statement/00_intro.sql
@@ -13,3 +13,18 @@ select  EmployeeID,
 
 From Employees
 
+
+
+
+
+SELECT SaleId, SaleAmount,
+
+	CASE 
+		WHEN SaleAmount <= 100 THEN 'Weak'
+		WHEN SaleAmount BETWEEN 101 AND 200 THEN 'Good'
+		WHEN SaleAmount BETWEEN 201 AND 300 THEN 'Very Good'
+		WHEN SaleAmount > 300 THEN 'Exellent'
+		ELSE 'NOT SPECIFIED'
+
+		END AS [Sale Level] 
+FROM Sales
