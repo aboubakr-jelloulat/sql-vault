7673333d0676f65c52ceb00e6047d7745e4aeb43
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-08-22 16:14:04 +0100

    Aggregate Functions

--- T-SQL/09_Buil-in functions/02_Aggregate_Functions.sql
+++ T-SQL/09_Buil-in functions/02_Aggregate_Functions.sql
@@ -1,1 +1,32 @@
 
+
+USE C21_DB1
+
+SELECT Department, COUNT(*) AS EmployeeCount
+FROM Employees2
+GROUP BY Department;
+
+
+SELECT Department, SUM(Salary) AS TotalSalary
+FROM Employees2
+GROUP BY Department;
+
+
+
+SELECT Department, AVG(PerformanceRating) AS AvgPerformanceRating
+FROM Employees2
+GROUP BY Department;
+
+
+
+SELECT MIN(Salary) AS LowestSalary
+FROM Employees2;
+
+
+
+SELECT MAX(Salary) AS HighestSalary
+FROM Employees2;
+
+
+
+
