1d76f8fd8519984de451a749cb7b4a072d9dab83
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-08-24 21:21:51 +0100

    Inline Table-Valued Functions (ITVFs) in T-SQL

--- T-SQL/12_Table-Valued Functions/01_ITVF.sql
+++ T-SQL/12_Table-Valued Functions/01_ITVF.sql
@@ -4,6 +4,8 @@ USE C21_DB1;
 GO
 
 
+--  Inline Table-Valued Functions (ITVFs) in T-SQL
+
 
 CREATE FUNCTION GetStudentsBySubject(@Subject NVARCHAR(50))		RETURNS TABLE
 
@@ -27,5 +29,20 @@ GO
 
 
 	SELECT AVG(Grade)  FROM dbo.GetStudentsBySubject('Science')
+
+
+
+
+
+----   P2 :   Using Inline Table-Valued Functions with JOIN in T-SQL
+
+
+
+
+
+SELECT s.StudentID, s.NAME AS [Student Name], t.Name AS [Theacher Name], s.Grade
+	
+	FROM   dbo.GetStudentsBySubject('Math') s
+	INNER JOIN Teachers t ON s.Subject = t.Subject
 
 
