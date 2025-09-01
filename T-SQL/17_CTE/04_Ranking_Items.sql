b527b189f45f3f07d382c9719ef796c72d64e7d9
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-09-01 17:07:58 +0100

    Ranking Items

---
+++ T-SQL/17_CTE/04_Ranking_Items.sql
@@ -0,0 +1,49 @@
+
+
+CREATE TABLE SalesRecords (
+    RecordID INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeID INT,
+    SaleAmount DECIMAL(10, 2),
+    SaleDate DATE
+);
+
+INSERT INTO SalesRecords (EmployeeID, SaleAmount, SaleDate) VALUES
+(1, 1200.50, '2023-01-10'),
+(2, 800.00, '2023-01-11'),
+(1, 600.25, '2023-01-15'),
+(3, 950.75, '2023-01-20'),
+(2, 400.00, '2023-01-21'),
+(1, 300.00, '2023-01-25'),
+(3, 1200.00, '2023-01-30'),
+(2, 750.00, '2023-02-05'),
+(1, 500.00, '2023-02-10'),
+(3, 850.00, '2023-02-15'),
+(4, 3000.75, '2023-02-15');
+
+
+
+USE C21_DB1;
+
+GO
+
+WITH SalesTotals AS 
+(
+    SELECT 
+        EmployeeID,  SUM(SaleAmount) AS TotalSales  FROM SalesRecords    GROUP BY EmployeeID
+),  
+
+RankedSales AS 
+(
+    SELECT  EmployeeID,  TotalSales,  RANK() OVER (ORDER BY TotalSales DESC) AS SalesRank   FROM SalesTotals
+)
+
+SELECT EmployeeID, TotalSales, SalesRank   FROM RankedSales;
+
+
+
+
+--RANK() → generate rank numbers.
+
+--OVER (...) → tells SQL how to rank.
+
+--ORDER BY TotalSales DESC → highest sales get rank 1, then rank 2, etc. (DESC = descending).
